fd7e0cbfb858970dc3cac761e3b0a6793dcccaa4
NelsonHernandezDiaz <80290775+NelsonHernandezDiaz@users.noreply.github.com>
2021-06-24 21:49:47 -0300

    Add files via upload

---
+++ Esquirlas.Domain/Enums/DiosesEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquirlas.Domain.Enums
+{
+    public enum DiosesEnum
+    {
+        //TODO: Agregar el nombre de los DIOSES
+        Farna,
+        Horomir,
+        De_las_Mentiras
+    }
+}
