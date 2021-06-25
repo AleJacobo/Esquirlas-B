509e5d14cf7a77489dfb4c2ab260d15555e2e9ad
NelsonHernandezDiaz <80290775+NelsonHernandezDiaz@users.noreply.github.com>
2021-06-24 21:49:47 -0300

    Add files via upload

---
+++ Esquirlas.Domain/Common/CiudadesDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esquirlas.Domain.Common
+{
+    public static class CiudadesDictionary
+    {
+        public static readonly Dictionary<int, string> Ciudades = new Dictionary<int, string>()
+        {
+            //TODOS: Agregar el nombre de las ciudades en el string
+            { 1, " " },
+            { 2, " " },
+            { 3, " " },
+            { 4, " " },
+            { 5, " " },
+            { 6, " " },
+            { 7, " " },
+            { 8, " " },
+            { 9, " " },
+        };
+    }
+}
