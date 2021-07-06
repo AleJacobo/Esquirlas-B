3242fe3ad996a9f9ecca7af07ee784d098c25597
NelsonHernandezDiaz <80290775+NelsonHernandezDiaz@users.noreply.github.com>
2021-07-05 23:29:54 -0300

    Update MappFacciones.cs

--- Esquirlas.Application/AutoMappers/MappFacciones.cs
+++ Esquirlas.Application/AutoMappers/MappFacciones.cs
@@ -13,7 +13,8 @@ namespace Esquirlas.Application.AutoMappers
     {
         public MappFacciones()
         {
-            CreateMap<FaccionesDTO, Faccion>()
+            CreateMap<FaccionesDTO, Faccion>().ReverseMap();
+            //Se puede personalizar para cuando no usas los mismos datos en cada clase o cuando se llaman distinto pero es lo mismo (ejemplo dos idiomas)
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
                 .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => src.IsDeleted));                
