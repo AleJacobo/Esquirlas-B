f59c85cf57f0e341108173f6b17d2bf05e97706a
NelsonHernandezDiaz <80290775+NelsonHernandezDiaz@users.noreply.github.com>
2021-07-05 23:24:04 -0300

    Add files via upload

--- Esquirlas.Domain/Entities/User.cs
+++ Esquirlas.Domain/Entities/User.cs
@@ -12,11 +12,13 @@ namespace Esquirlas.Domain.Entities
         public string UserName { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
+        public int Age { get; set; }
         public DateTime Birthday { get; set; }
         public int Telephone { get; set; }
         public string PhotoUrl { get; set; }
         public string Email { get; set; }
         public string Nationality { get; set; }
+        public string Status { get; set; }
         public bool IsDeleted { get; set; }
 
         //para heredar los enums
